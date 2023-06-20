9bbe26c33250ebcd52935435aba6f260d60414c7
Fefe Moyses <68619811+fernandagmo@users.noreply.github.com>
2023-06-20 19:10:32 -0300

    Update detalhes.js

--- detalhes.js
+++ detalhes.js
@@ -1,26 +1,2 @@
-window.onload = () => {
-  lerInfos();
-};
 
-let divDetalhes = document.getElementById('detalhes');
-let btvoltar = document.getElementById('voltar');
 
-btvoltar.addEventListener('click', () => {
-  window.history.back();
-});
-
-function lerInfos() {
-  divDetalhes.innerHTML = `
-    <div class="container">
-      <div class="image">
-        <img src="${localStorage.getItem('@Imagem')}">
-      </div>
-      <div class="info">
-        <h1>${localStorage.getItem('@Nome')}</h1>
-        <h2>${localStorage.getItem('@Posicao')}</h2>
-        <p>${localStorage.getItem('@Descricao')}</p>
-        <strong>Nome completo: </strong><span>${localStorage.getItem('@Nome')}</span>
-      </div>
-    </div>
-  `;
-}
